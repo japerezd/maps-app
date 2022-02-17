1badc287b5ae7b4063e3d0be34278a63aedcef8d
Jorge Perez <jorge_alberto1997@hotmail.com>
2022-02-17 08:23:39 -0600

    Store distance and duration

--- src/store/map/mutations.ts
+++ src/store/map/mutations.ts
@@ -8,6 +8,15 @@ const mutation: MutationTree<MapState> = {
     state.map = map;
   },
 
+  setDistanceDuration(state, { distance, duration }: { distance: number, duration: number }) {
+    let kms = distance / 1000;
+        kms = Math.round(kms * 100);
+        kms /= 100;
+    state.distance = kms;
+
+    state.duration = Math.floor(duration / 60)
+  },
+
   setPlaceMarkers(state, places: Feature[]) {
     // Borrar marcadores
     state.markers.forEach((marker) => marker.remove());
